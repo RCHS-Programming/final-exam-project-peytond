85141dcd61162b91e40766c151f73e3519e86d9f
peytond <peyton.dunn@stu.boone.kyschools.us>
2021-05-13 08:37:38 -0400

    added score board, almost finished with maze

--- playerWorld.java
+++ playerWorld.java
@@ -17,7 +17,9 @@ public class playerWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-
+        
+        addObject(new Castle(), 510, 410); 
+        addObject(new ScorePanel(), 76, 540); 
         for(int i = 0; i < 16; i++)
         {
             addObject(new Wall("edge.png"), 25 + 50*i, 12);
@@ -72,10 +74,6 @@ public class playerWorld extends World
         }
         for(int i = 0; i < 2; i++)
         {
-            addObject(new Wall("edge2.png"), 672, 100); 
-        }
-        for(int i = 0; i < 2; i++)
-        {
             addObject(new Wall("edge2.png"), 673, 205); 
         }
         for(int i = 0; i < 2; i++)
@@ -86,17 +84,126 @@ public class playerWorld extends World
         {
             addObject(new Wall("edge2.png"), 558, 260); 
         }
-        for(int i = 0; i < 2; i++)
-        {
-            addObject(new Wall("edge.png"), 571, 273); 
-        }
-        for(int i = 0; i < 2; i++)
-        {
-            addObject(new Wall("edge.png"), 755, 272); 
-        }
+        
         for(int i = 0; i < 2; i++)
         {
             addObject(new Wall("edge2.png"), 397, 124); 
+        }       
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 558, 408); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 558, 310); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 156); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 251); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 558, 360); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 711, 263); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 761, 263); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 558, 408); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 300); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 558, 455); 
+        }
+        /*
+         * New Section - 
+         */
+        for (int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 348); 
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 395); 
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 554); 
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 673, 429); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 521, 467);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 474, 467); 
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 461, 431);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 461, 382);
+        } 
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 461, 333);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 461, 283);
+        }
+        //Moving on
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 426, 270);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 377, 270);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge.png"), 327, 270);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 327, 556);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 327, 506);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 327, 459);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 327, 412);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            addObject(new Wall("edge2.png"), 327, 383);
         }
     }
 }
