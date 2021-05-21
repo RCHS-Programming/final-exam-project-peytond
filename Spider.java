7c21770ab49f2862a8468b5363f724067df86422
peytond <peyton.dunn@stu.boone.kyschools.us>
2021-05-21 08:09:31 -0400

    FINISHED

--- Spider.java
+++ Spider.java
@@ -17,16 +17,24 @@ public class Spider extends Actor
         randomTurn(); 
         detectWall(); 
         move();
+        turnAtEdge();
     }    
     public void move()
     {
         move(1); 
     }
+    public void turnAtEdge()
+    {
+        if(isAtEdge() )
+        {
+            turn(4);
+        }
+    }
     public void randomTurn()
     {
         if(Greenfoot.getRandomNumber(100) < 10)
         {
-            turn(Greenfoot.getRandomNumber(100) - 60);
+            turn(Greenfoot.getRandomNumber(91) - 60);
         }
     }
     public void detectWall()
